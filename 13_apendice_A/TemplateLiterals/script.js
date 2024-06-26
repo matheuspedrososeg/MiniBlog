1c6b2df0104e0e003ef32729204eaa985f838309
Gustavo Biazus Priftis <gustavopriftis727@gmail.com>
2024-06-26 10:46:15 -0300

    Apendice a, relacionado a novas alterações que vieram no ES6

---
+++ 13_apendice_A/TemplateLiterals/script.js
@@ -0,0 +1,6 @@
+// 5 templates literals
+const userName = "Matheus";
+const age = 30;
+
+console.log(`O nome do usuário é ${userName} e tem ${age} anos.`);
+console.log("O nome do usuário é " + userName + " e tem " + age + " anos.");
